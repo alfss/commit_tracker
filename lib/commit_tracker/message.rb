aa4c04032a0b40883d3ca262a78e914e48adae1c
Sergey V. Kravchuk <alfss.obsd@gmail.com>
2011-12-04 01:28:24 +0400

    add class task and message

---
+++ lib/commit_tracker/message.rb
@@ -0,0 +1,33 @@
+require "commit_tracker/error"
+require "commit_tracker/trackstudio"
+
+module CommitTracker
+  class Message
+    def initialize(options={})
+      @type   = options[:type] || 'trackstudio'
+      @commit = nil
+      case @type
+      when 'trackstudio'
+        @commit = TrackStudio.new(options)
+      end
+    end
+    
+    def create_message(options={})
+      case @type
+      when 'trackstudio'
+        return @commit.create_message(options)
+      else
+        raise ErrorCommitTask, "error name tracker"
+      end
+    end
+    
+    def delete_message(options={})
+      case @type
+      when 'trackstudio'
+        return @commit.delete_message(options)
+      else
+        raise ErrorCommitTask, "error name tracker"
+      end
+    end
+  end
+end
---
+++ lib/commit_tracker/task.rb
@@ -0,0 +1,33 @@
+require "commit_tracker/error"
+require "commit_tracker/trackstudio"
+
+module CommitTracker
+  class Task
+    def initialize(options={})
+      @type   = options[:type] || 'trackstudio'
+      @commit = nil
+      case @type
+      when 'trackstudio'
+        @commit = TrackStudio.new(options)
+      end
+    end
+    
+    def create_task(options={})
+      case @type
+      when 'trackstudio'
+        return @commit.create_task(options)
+      else
+        raise ErrorCommitTask, "error name tracker"
+      end
+    end
+    
+    def delete_task(options={})
+      case @type
+      when 'trackstudio'
+        return @commit.delete_task(options)
+      else
+        raise ErrorCommitTask, "error name tracker"
+      end
+    end
+  end
+end
